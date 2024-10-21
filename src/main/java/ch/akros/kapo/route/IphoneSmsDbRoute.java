a2529bfbfe578eb362eb03c3ff42967e92a3d7bf
Assen Sharlandjiev <assen.sharlandjiev@akros.ch>
2024-10-21 16:57:49 +0300

    added foundation for the iphone chat extract.

--- src/main/java/ch/akros/kapo/route/IphoneSmsDbRoute.java
+++ src/main/java/ch/akros/kapo/route/IphoneSmsDbRoute.java
@@ -2,10 +2,16 @@ package ch.akros.kapo.route;
 
 import static org.apache.camel.Exchange.FILE_NAME;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
+import ch.akros.kapo.service.IphoneMessagesDbParser;
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class IphoneSmsDbRoute extends AbstractRouteBuilder {
 
   IphoneSmsDbRoute(final ApplicationArguments arguments) {
@@ -17,6 +23,7 @@ public class IphoneSmsDbRoute extends AbstractRouteBuilder {
     final var outoutPath = getTargetPath("sqllite");
     final var toFile = "file:".concat(outoutPath);
     from("direct:iphoneSmsDbRoute")
+        .process(iphoneMessageDbProcessor())
         .setBody(header("fileMetadata"))
         .setHeader(FILE_NAME, header("fileMetadataJsonFileName"))
         .marshal().json()
@@ -24,6 +31,18 @@ public class IphoneSmsDbRoute extends AbstractRouteBuilder {
         .setBody(header("TikaText"))
         .setHeader(FILE_NAME, header("tikaTextFileName"))
         .to(toFile);
+  }
+
+  private Processor iphoneMessageDbProcessor() {
+    return e -> {
+      final var dbFilePath = e.getIn().getHeader(Exchange.FILE_PATH, String.class);
+      try {
+        final var parser = new IphoneMessagesDbParser(dbFilePath);
+        parser.parse();
+      } catch (final Exception ex) {
+        log.error(ex.getMessage(), ex);
+      }
+    };
   }
 
 }
